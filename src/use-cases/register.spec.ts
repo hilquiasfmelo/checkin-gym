9ed9330f424bdff81a50fbbaad5d9e72ca9dac8c
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2023-04-10 20:41:56 -0300

    Commit: Design Patterns & Testes - Utilizando UI do Vitest

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,60 @@
+import { describe, expect, it } from 'vitest'
+import { compare } from 'bcryptjs'
+
+import { RegisterUseCase } from './register'
+import { InMemoryRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+describe('Register Use Case', async () => {
+  it('should be able register to new user', async () => {
+    const inMemoryRepository = new InMemoryRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryRepository)
+
+    const { user } = await registerUseCase.run({
+      name: 'John Dow',
+      email: 'johndoe@example.com',
+      password: '12345678',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should be able hash user password upon registration', async () => {
+    const inMemoryRepository = new InMemoryRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryRepository)
+
+    const { user } = await registerUseCase.run({
+      name: 'John Dow',
+      email: 'johndoe@example.com',
+      password: '12345678',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '12345678',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should be able to register with same email twice', async () => {
+    const inMemoryRepository = new InMemoryRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryRepository)
+
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.run({
+      name: 'John Dow',
+      email,
+      password: '12345678',
+    })
+
+    expect(async () => {
+      await registerUseCase.run({
+        name: 'John Dow',
+        email,
+        password: '12345678',
+      })
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { hash } from 'bcryptjs'
+import type { User } from '@prisma/client'
 
 import { UsersRepository } from '@/repositories/interfaces/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
@@ -9,10 +10,18 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async run({ name, email, password }: RegisterUseCaseRequest) {
+  async run({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 4)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -21,10 +30,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
